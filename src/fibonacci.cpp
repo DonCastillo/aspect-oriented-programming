6dc37eb989c04ec7258de9c77dda5bc587957ebf
DonCastillo <don.qcastillo@yahoo.com>
2022-10-27 22:07:11 -0600

    implemented fibonacci and it works

--- src/fibonacci.cpp
+++ src/fibonacci.cpp
@@ -1,5 +1,10 @@
 #include "fibonacci.h"
 
 uint64_t fibonacci(unsigned int n) {
+  if(n == 0 || n == 1 || n == 2) {
+    return 1;
+  } else {
+    return fibonacci(n - 1) + fibonacci(n - 2); 
+  }
   return -1;
 }
