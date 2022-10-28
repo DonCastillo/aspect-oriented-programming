aa3587bf466e46a8b904d0f23e30ca5c327d2c60
DonCastillo <don.qcastillo@yahoo.com>
2022-10-28 10:03:28 -0600

    fixed styles

--- src/fibonacci.cpp
+++ src/fibonacci.cpp
@@ -1,10 +1,10 @@
-#include "fibonacci.h"
+#include "../include/fibonacci.h"
 
 uint64_t fibonacci(unsigned int n) {
-  if(n == 0 || n == 1 || n == 2) {
+  if (n == 0 || n == 1 || n == 2) {
     return 1;
   } else {
-    return fibonacci(n - 1) + fibonacci(n - 2); 
+    return fibonacci(n - 1) + fibonacci(n - 2);
   }
   return -1;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,10 @@
-
-#include "fibonacci.h"
 #include <iostream>
+#include "../include/fibonacci.h"
 
 int main(int argc, char const *argv[]) {
   std::string s(argv[1]);
   unsigned int N = std::stoi(s);
-  long long x = fibonacci(N);
+  uint64_t x = fibonacci(N);
   std::cout << "The " << N << "th Fibonacci number is: " << x << std::endl;
   return 0;
 }
